d71d1ad4540f1b84edd056593a9d47a139085cc3
ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
2025-04-11 15:11:27 +0900

    ios 위젯 추가

---
+++ ios/MeditationBlossomWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  MeditationBlossomWidget
+//
+//  Created by 최상준 on 4/11/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ ios/MeditationBlossomWidget/MeditationBlossomWidgetLiveActivity.swift
@@ -0,0 +1,80 @@
+//
+//  MeditationBlossomWidgetLiveActivity.swift
+//  MeditationBlossomWidget
+//
+//  Created by 최상준 on 4/11/25.
+//
+
+import ActivityKit
+import WidgetKit
+import SwiftUI
+
+struct MeditationBlossomWidgetAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        // Dynamic stateful properties about your activity go here!
+        var emoji: String
+    }
+
+    // Fixed non-changing properties about your activity go here!
+    var name: String
+}
+
+struct MeditationBlossomWidgetLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: MeditationBlossomWidgetAttributes.self) { context in
+            // Lock screen/banner UI goes here
+            VStack {
+                Text("Hello \(context.state.emoji)")
+            }
+            .activityBackgroundTint(Color.cyan)
+            .activitySystemActionForegroundColor(Color.black)
+
+        } dynamicIsland: { context in
+            DynamicIsland {
+                // Expanded UI goes here.  Compose the expanded UI through
+                // various regions, like leading/trailing/center/bottom
+                DynamicIslandExpandedRegion(.leading) {
+                    Text("Leading")
+                }
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text("Trailing")
+                }
+                DynamicIslandExpandedRegion(.bottom) {
+                    Text("Bottom \(context.state.emoji)")
+                    // more content
+                }
+            } compactLeading: {
+                Text("L")
+            } compactTrailing: {
+                Text("T \(context.state.emoji)")
+            } minimal: {
+                Text(context.state.emoji)
+            }
+            .widgetURL(URL(string: "http://www.apple.com"))
+            .keylineTint(Color.red)
+        }
+    }
+}
+
+extension MeditationBlossomWidgetAttributes {
+    fileprivate static var preview: MeditationBlossomWidgetAttributes {
+        MeditationBlossomWidgetAttributes(name: "World")
+    }
+}
+
+extension MeditationBlossomWidgetAttributes.ContentState {
+    fileprivate static var smiley: MeditationBlossomWidgetAttributes.ContentState {
+        MeditationBlossomWidgetAttributes.ContentState(emoji: "😀")
+     }
+     
+     fileprivate static var starEyes: MeditationBlossomWidgetAttributes.ContentState {
+         MeditationBlossomWidgetAttributes.ContentState(emoji: "🤩")
+     }
+}
+
+#Preview("Notification", as: .content, using: MeditationBlossomWidgetAttributes.preview) {
+   MeditationBlossomWidgetLiveActivity()
+} contentStates: {
+    MeditationBlossomWidgetAttributes.ContentState.smiley
+    MeditationBlossomWidgetAttributes.ContentState.starEyes
+}
