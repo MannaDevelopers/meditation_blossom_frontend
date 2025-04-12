f80a93e6105821da769ac0e34c3b6b7d365cc279
SeminJang <pillar3500@gmail.com>
2025-04-12 14:18:21 +0900

    ios firebase 세팅 #2 데이터 받아오기 완료

--- ios/meditation_blossom/AppDelegate.h
+++ ios/meditation_blossom/AppDelegate.h
@@ -1,6 +1,7 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
-#import <FirebaseCore>;
+//#import <FirebaseCore>
+#import <FirebaseCore/FirebaseCore.h>
 
 @interface AppDelegate : RCTAppDelegate
 
