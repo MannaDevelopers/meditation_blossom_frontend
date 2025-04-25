e6a3b830045db5e76315260675a6202b62031536
noni1016 <51592258+noni1016@users.noreply.github.com>
2025-04-25 09:33:12 +0900

    [SCRUM-17] [RN] 서버에서 데이터 받기. (#10)
    * ios firebase 세팅 #1
    
    * ios firebase 세팅 #2 데이터 받아오기 완료
    
    * [SCRUM-17] App.tsx 를 src 폴더 안으로 옮기고 App.tsx 내용 TempSermonScreen 으로 옮김.
    
    ---------
    
    Co-authored-by: SeminJang <pillar3500@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 import HomeScreen from './screens/HomeScreen';
-import EditScreen from './screens/EditScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { RootStackParamList } from './types/navigation';
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
-const RootStack = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="HomeScreen" 
-          component={HomeScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="EditScreen" 
-          component={EditScreen} 
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-        name="SettingsScreen"
-        component={SettingsScreen}
-        options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
+import TempSermonScreen from './screens/TempSermonScreen';
 
 function App(): React.JSX.Element {
 
   return (
-    <RootStack />
+    <TempSermonScreen/>
+    // <HomeScreen/>
   );
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,45 +1,8 @@
-import { View, Text, StyleSheet, ScrollView, Image, Button, TouchableOpacity, ImageBackground } from 'react-native';
-import WidgetPreview from '../components/WidgetPreview';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-// import Icon from 'react-native-vector-icons/AntDesign';
-import { RootStackParamList } from '../types/navigation';
-
-type Props = NativeStackScreenProps<RootStackParamList, 'HomeScreen'>;
-
-
-const HomeScreen = ({navigation}: Props) => {
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: 'transparent', marginHorizontal: 35, marginVertical: 35, justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{ backgroundColor: 'transparent', flex: 1 }}>
-        <View style={{ backgroundColor: 'transparent', flexDirection: 'row', width: 305, height: 30, marginBottom: 35, alignItems: 'center'}}>
-          <Image source={require('../assets/image/20250416_meditation_icon.png')} style={{ backgroundColor: 'transparent', borderRadius: 15, width: 20, height: 20 }} />
-          <Text style={{ color: '#49454F', fontSize: 20, letterSpacing: -1, fontFamily: "Pretendard-Medium", marginLeft: 8}}>묵상만개</Text>
-          {/* <Icon name="setting" size={20} color="#49454F" style={{ marginLeft: 'auto' }} /> */}
-          <TouchableOpacity onPress={() => navigation.navigate('SettingsScreen')} style={{ marginLeft: 'auto' }}>
-            <Image source={require('../assets/image/Settings.png')} style={{ backgroundColor: 'transparent', borderRadius: 15, width: 20, height: 20 }} />
-          </TouchableOpacity>
-        </View>
-        <View style={{ backgroundColor: 'transparent', justifyContent: 'center', alignItems: 'center', width: 305, height: 25 }}>
-          <Text style={{ color: "#A59EAE", fontSize: 20, letterSpacing: -3, fontFamily: "Pretendard-SemiBold" }}>2025-03-23</Text>
-        </View>
-        <View style={{ backgroundColor: 'transparent', justifyContent: 'center', alignItems: 'center', width: 305, height: 30 }}>
-          <Text style={{ color: "#A59EAE", fontSize: 24, letterSpacing: -3, fontFamily: "Pretendard-Bold" }}>누룩, 복음과 고난을 받으라</Text>
-        </View>
-        <View style={{ backgroundColor: 'transparent', width: 305, height: 300, justifyContent: 'center', alignItems: 'center' }}>
-          <WidgetPreview />
-        </View>
-        <View style={{ backgroundColor: 'transparent', width: 305, height: 38, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
-          <TouchableOpacity onPress={() => navigation.navigate('EditScreen')}>
-            <ImageBackground
-              source={require('../assets/image/EditButton.png')}
-              style={{ width: 62, height: 38, borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}
-              imageStyle={{ borderRadius: 10 }}
-            >
-              <Text style={{ color: 'white', fontSize: 20, fontFamily: "Pretendard-Bold", textAlign: 'center', letterSpacing: -1 }}>편집</Text>
-            </ImageBackground>
-          </TouchableOpacity>
-        </View>
-      </View>
+    <View>
+<Text>HomeScreen</Text>
     </View>
   );
 }
