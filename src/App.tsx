902e8d9e780135377c60f5856843d898f7407a4e
noni1016 <play.noni@gmail.com>
2025-05-03 16:34:42 +0900

    [SCRUM-32] 편집페이지, 설정페이지 연결 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import firestore from '@react-native-firebase/firestore';
 import HomeScreen from './screens/HomeScreen';
-import TempSermonScreen from './screens/TempSermonScreen';
 import EditScreen from './screens/EditScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from './types/navigation';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const RootStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="HomeScreen" 
+          component={HomeScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="EditScreen" 
+          component={EditScreen} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+        name="SettingsScreen"
+        component={SettingsScreen}
+        options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
 function App(): React.JSX.Element {
 
   return (
-    // <EditScreen/>
-    // <TempSermonScreen/>
-    <View style={{flex: 1, backgroundColor: 'white'}}>
-    <HomeScreen/>
-    </View>
-    // <Text>Hello</Text>
+    <RootStack />
   );
 }
 
--- src/screens/EditScreen.tsx
+++ src/screens/EditScreen.tsx
@@ -1,13 +1,13 @@
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import WidgetPreview from '../components/WidgetPreview';
+import { RootStackParamList } from '../types/navigation';
 
 const EditScreen = () => {
   return (
-    <View style={{flex: 1, backgroundColor: 'yellow', marginHorizontal: 35, marginVertical: 60, justifyContent: 'center', alignItems: 'center'}}>
-      <View style={{backgroundColor: 'blue', marginVertical: 105, borderRadius: 10}} >
-<WidgetPreview/>
-</View>
-
+    <View style={{ flex: 1, backgroundColor: 'yellow', marginHorizontal: 35, marginVertical: 60, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ backgroundColor: 'blue', marginVertical: 105, borderRadius: 10 }} >
+        <WidgetPreview />
+      </View>
     </View>
   );
 }
