cab56de6a299b00599068190c66ae55e39bfc86e
noni1016 <play.noni@gmail.com>
2025-05-04 07:36:24 +0900

    [SCRUM-32] svg 아이콘 사용할 수 있도록 패키지 설정

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,1 @@
+export { default as TextLeft } from './TextLeft.svg';
--- src/screens/EditScreen.tsx
+++ src/screens/EditScreen.tsx
@@ -1,13 +1,20 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import WidgetPreview from '../components/WidgetPreview';
 import { RootStackParamList } from '../types/navigation';
+import SvgIcon from '../components/SvgIcon';
+
 
 const EditScreen = () => {
   return (
     <View style={{ flex: 1, backgroundColor: 'yellow', marginHorizontal: 35, marginVertical: 60, justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{ backgroundColor: 'blue', marginVertical: 105, borderRadius: 10 }} >
+      <View style={{ backgroundColor: 'blue', marginVertical: 105, borderRadius: 20 }} >
         <WidgetPreview />
       </View>
+      {/*Edit Tab*/}
+      {/*Tab 1*/}
+      <View style={{ backgroundColor: 'red', width: 305, marginHorizontal: 64, marginVertical: 55}}>
+        <SvgIcon name="TextLeft" size={24} onPress={() => {}} />
+        </View>
     </View>
   );
 }
