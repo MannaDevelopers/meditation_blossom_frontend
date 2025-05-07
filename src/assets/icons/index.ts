327cb30ef9634013c6d33aacd2e0ef36ed7dd7f0
noni1016 <51592258+noni1016@users.noreply.github.com>
2025-05-07 13:39:18 +0900

    [SCRUM-32] [RN] 전시옵션 편집 버튼 (#13)
    * [SCRUM-29] 메인페이지 레이아웃 구성
    
    * [SCRUM-29] 메인페이지 레이아웃 구성 - 편집 버튼 추가
    
    * [SCRUM-29] Settings icon 추가. vector icon 적용 안돼서 이미지 파일 사용함
    
    * [SCRUM-29] WidgetPreview 바탕색 제거
    
    * [SCRUM-32] 편집페이지, 설정페이지 연결 추가
    
    * [SCRUM-32] svg 아이콘 사용할 수 있도록 패키지 설정
    
    * [SCRUM-32] 편집 페이지 아이콘 배치
    
    * [SCRUM-32] 뒤로가기, 저장 버튼 추가
    
    ---------
    
    Co-authored-by: SeminJang <pillar3500@gmail.com>

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as TextLeft } from './TextLeft.svg';
+export { default as TextCenter } from './TextCenter.svg';
+export { default as TextRight } from './TextRight.svg';
+export { default as EditText } from './EditText.svg';
+export { default as EditStyle } from './EditStyle.svg';
+export { default as EditBackground } from './EditBackground.svg';
+export { default as BackButton } from './BackButton.svg';
