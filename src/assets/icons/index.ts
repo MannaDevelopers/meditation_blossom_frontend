90a3247e321043ce9da7fe60dc2c6fbeb7f49fb6
SeminJang <pillar3500@gmail.com>
2025-05-07 12:59:13 +0900

    [SCRUM-32] 편집 페이지 아이콘 배치

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,1 +1,6 @@
 export { default as TextLeft } from './TextLeft.svg';
+export { default as TextCenter } from './TextCenter.svg';
+export { default as TextRight } from './TextRight.svg';
+export { default as EditText } from './EditText.svg';
+export { default as EditStyle } from './EditStyle.svg';
+export { default as EditBackground } from './EditBackground.svg';
