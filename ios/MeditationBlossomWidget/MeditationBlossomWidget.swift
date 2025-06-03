9ca161759cbbe5768b4fdac07bc2e78cdd1651c5
ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
2025-06-03 12:04:28 +0900

    ios 위젯 배경 적용

--- ios/MeditationBlossomWidget/MeditationBlossomWidget.swift
+++ ios/MeditationBlossomWidget/MeditationBlossomWidget.swift
@@ -8,81 +8,69 @@
 import WidgetKit
 import SwiftUI
 
-struct Provider: AppIntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
-    }
-
-    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: configuration)
-    }
-    
-    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        return Timeline(entries: entries, policy: .atEnd)
-    }
-
-//    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
-}
-
 struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let configuration: ConfigurationAppIntent
+  let date: Date;
 }
 
-struct MeditationBlossomWidgetEntryView : View {
-    var entry: Provider.Entry
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
 
-    var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        completion(SimpleEntry(date: Date()))
+    }
 
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
-        }
+    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
+        let currentDate = Date()
+        let nextUpdate = Calendar.current.date(byAdding: .minute, value: 15, to: currentDate)!
+        let entry = SimpleEntry(date: currentDate)
+        let timeline = Timeline(entries: [entry], policy: .after(nextUpdate))
+        completion(timeline)
     }
 }
+
 
 struct MeditationBlossomWidget: Widget {
     let kind: String = "MeditationBlossomWidget"
 
-    var body: some WidgetConfiguration {
-        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
+  var body: some WidgetConfiguration {
+          StaticConfiguration(kind: "MyWidget", provider: Provider()) { entry in
             MeditationBlossomWidgetEntryView(entry: entry)
-                .containerBackground(.fill.tertiary, for: .widget)
-        }
+              .containerBackground(.fill.tertiary, for: .widget)
+          }
+        .supportedFamilies([.systemSmall, .systemMedium])
     }
 }
 
-extension ConfigurationAppIntent {
-    fileprivate static var smiley: ConfigurationAppIntent {
-        let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "😀"
-        return intent
+struct MeditationBlossomWidgetEntryView : View {
+  var entry: SimpleEntry
+  @Environment(\.widgetFamily) var family
+  
+  var body: some View {
+    switch family {
+    case .systemSmall:
+      ZStack {
+        Image("background_364_382")
+          .resizable()
+          .frame(width:130, height:290/3)
+          .offset(y:15)
+      }
+    case .systemMedium:
+      ZStack {
+        Image("background_364_170")
+          .resizable()
+          .frame(width:364, height:170)
+      }
+    default:
+      Text("Error occured")
     }
-    
-    fileprivate static var starEyes: ConfigurationAppIntent {
-        let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "🤩"
-        return intent
-    }
+  }
 }
 
 #Preview(as: .systemSmall) {
     MeditationBlossomWidget()
 } timeline: {
-    SimpleEntry(date: .now, configuration: .smiley)
-    SimpleEntry(date: .now, configuration: .starEyes)
+    SimpleEntry(date: .now)
+    SimpleEntry(date: .now)
 }
