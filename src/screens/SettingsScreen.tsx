a9cb218c7700adae8d83c6e9d9ad06e7badafc9d
noni1016 <51592258+noni1016@users.noreply.github.com>
2025-06-06 10:47:22 +0900

    [SCRUM-29] [RN] 메인 화면에 말씀 데이터 표시 (#17)
    * [SCRUM-29] 메인페이지 레이아웃 구성
    
    * [SCRUM-29] 메인페이지 레이아웃 구성 - 편집 버튼 추가
    
    * [SCRUM-29] Settings icon 추가. vector icon 적용 안돼서 이미지 파일 사용함
    
    * [SCRUM-29] WidgetPreview 바탕색 제거
    
    * [SCRUM-32] 편집페이지, 설정페이지 연결 추가
    
    * update ios Podfile.lock
    
    * [SCRUM-29] AsyncStorage 에 저장된 말씀으로 본문 내용 표시
    
    * [SCRUM-29] 데이터 가져오는 임시 버튼 생성
    
    * [SCRUM-29] 임시 버튼 Settings 페이지로 위치 변경
    
    * [SCRUM-29] 화면에 표시할 데이터는 display_sermon key 로 따로 저장
    
    * [SCRUM-29] 앱 최초 설치시 데이터가 없으면 바로 받아오도록 수정
    
    * https://mannachurchdev.atlassian.net/browse/SCRUM-36
    최신 설교 날짜로 부터 일주일이 지났을 경우 서버에 자동으로 요청하도록 수정
    
    * [SCRUM-29] pr comment 반영
    
    * [SCRUM-29] pr comment 반영 #2 - displaySermon 이 앱 시작할때마다 다시 저장되지 않도록 수정
    
    ---------
    
    Co-authored-by: SeminJang <pillar3500@gmail.com>

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,9 +1,65 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Sermon, SermonMetadata, STORAGE_KEY, METADATA_KEY } from '../types/Sermon';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types/navigation';
 
-const SettingsScreen = () => {
+type Props = NativeStackScreenProps<RootStackParamList, 'SettingsScreen'>;
+
+const SettingsScreen = ({navigation, route}: Props) => {
+  // 로컬 스토리지 비우기
+  const clearLocalStorage = async () => {
+    try {
+      await AsyncStorage.removeItem(STORAGE_KEY);
+      await AsyncStorage.removeItem(METADATA_KEY);
+      await AsyncStorage.removeItem('dispaly_sermon');
+
+      route.params.setSermons([]);
+      route.params.setLatestDate(null);
+      route.params.setMetadata({
+        latestDate: '',
+        lastUpdated: new Date().toISOString(),
+        totalCount: 0
+      });
+      route.params.setDisplaySermon(undefined);
+      
+      console.log('Local storage and metadata cleared');
+    } catch (error) {
+      console.error('Error clearing local storage:', error);
+    }
+  };
+
+  // 로컬 스토리지 내용 확인
+  const inspectStorage = async () => {
+    console.log('Inspecting AsyncStorage...');
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      
+      // 모든 키를 로그로 출력
+      console.log(keys);
+      
+      // 각 키에 대한 데이터 검사
+      for (const key of keys) {
+        const value = await AsyncStorage.getItem(key);
+        console.log([JSON.parse(value || '{}')]);
+      }
+    } catch (error) {
+      console.error('Error inspecting AsyncStorage:', JSON.stringify(error, null, 2));
+    }
+  };
+
   return (
-    <View style={{ flex: 1, backgroundColor: 'yellow', marginHorizontal: 35, marginVertical: 60, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, backgroundColor: 'black', marginHorizontal: 35, marginVertical: 60, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Settings Screen</Text>
+      <TouchableOpacity onPress={clearLocalStorage} style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5, marginTop: 20 }}>
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>Clear Local Storage</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={route.params.onRefresh} style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5, marginTop: 20 }}>
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>Refresh Data</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={inspectStorage} style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5, marginTop: 20 }}>
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}>Inspect Storage</Text>
+      </TouchableOpacity>
     </View>
   );
 }   
