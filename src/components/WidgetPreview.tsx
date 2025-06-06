1e3505b96c0061d79c8c901b8d469943c21fd2c9
noni1016 <51592258+noni1016@users.noreply.github.com>
2025-06-06 10:47:22 +0900

    [SCRUM-29] [RN] 메인 화면에 말씀 데이터 표시 (#17)
    * [SCRUM-29] 메인페이지 레이아웃 구성
    
    * [SCRUM-29] 메인페이지 레이아웃 구성 - 편집 버튼 추가
    
    * [SCRUM-29] Settings icon 추가. vector icon 적용 안돼서 이미지 파일 사용함
    
    * [SCRUM-29] WidgetPreview 바탕색 제거
    
    * [SCRUM-32] 편집페이지, 설정페이지 연결 추가
    
    * update ios Podfile.lock
    
    * [SCRUM-29] AsyncStorage 에 저장된 말씀으로 본문 내용 표시
    
    * [SCRUM-29] 데이터 가져오는 임시 버튼 생성
    
    * [SCRUM-29] 임시 버튼 Settings 페이지로 위치 변경
    
    * [SCRUM-29] 화면에 표시할 데이터는 display_sermon key 로 따로 저장
    
    * [SCRUM-29] 앱 최초 설치시 데이터가 없으면 바로 받아오도록 수정
    
    * https://mannachurchdev.atlassian.net/browse/SCRUM-36
    최신 설교 날짜로 부터 일주일이 지났을 경우 서버에 자동으로 요청하도록 수정
    
    * [SCRUM-29] pr comment 반영
    
    * [SCRUM-29] pr comment 반영 #2 - displaySermon 이 앱 시작할때마다 다시 저장되지 않도록 수정
    
    ---------
    
    Co-authored-by: SeminJang <pillar3500@gmail.com>

--- src/components/WidgetPreview.tsx
+++ src/components/WidgetPreview.tsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from 'react';
 import {View, Text, ImageBackground} from 'react-native';
+import { ScrollView, GestureHandlerRootView } from 'react-native-gesture-handler';
 
-const WidgetPreview = () => {
+const regex = /본문\s*[:：]?\s*([^\d\s]+ ?\d+:\d+(?:-\d+)?)/;
+const extractContent = (text: string) : { index: string; content: string } => {
+  const match = text.match(regex);
+  if (!match) {
+    return { index: '본문을 찾을 수 없습니다.', content: '' };
+  }
+  const verse = match[1].trim();
+  const contentStartIndex = text.indexOf(verse) + verse.length;
+  const content = text.slice(contentStartIndex).trim();
+
+  return {
+    index: verse,
+    content: content
+  }
+};
+
+const WidgetPreview = ({content}: {content: string | undefined}) => {
+  const [extractedContent, setExtractedContent] = useState<{ index: string; content: string }>({ index: '', content: '' });
+
+  useEffect(() => {
+    if (content) {
+      const { index, content: extractedContent } = extractContent(content);
+      setExtractedContent({ index, content: extractedContent });
+    }
+  }, [content]);
+
   return (
-    <View style={{width: 300, height: 270, backgroundColor: 'transparent', justifyContent: 'center', alignItems: 'center', borderRadius: 5}}>
+    <GestureHandlerRootView style={{width: 300, height: 270, backgroundColor: 'transparent', justifyContent: 'center', alignItems: 'center', borderRadius: 5}}>
       <ImageBackground source={require('../assets/image/BackgroundImg.png')} style={{backgroundColor: 'transparent', flex:1, borderRadius: 15}} >
-      <View style={{backgroundColor: 'transparent', marginVertical: 50, marginHorizontal: 30}}>
-        <Text style={{color: 'black', fontSize: 20, lineHeight: 24, fontWeight: 'bold', marginBottom: 30, fontFamily: "Pretendard-Regular"}}>또 비유로 말씀하시되 천국은 마치 여자가 가루 서 말 속에 갖다 넣어 전부 부풀게 한 누룩과 같으니라</Text>
-        <Text style={{color: 'black', fontSize: 16, fontFamily: "Pretendard-Regular"}}>마태복음 13:33</Text> 
-        </View>
-        </ImageBackground>
-        </View>
+        <ScrollView style={{backgroundColor: 'transparent', marginVertical: 50, marginHorizontal: 30}}>
+          <Text style={{color: 'black', fontSize: 20, lineHeight: 24, fontWeight: 'bold', marginBottom: 30, fontFamily: "Pretendard-Regular"}}>{extractedContent.content}</Text>
+          <Text style={{color: 'black', fontSize: 16, fontFamily: "Pretendard-Regular"}}>{extractedContent.index}</Text>
+        </ScrollView>
+      </ImageBackground>
+    </GestureHandlerRootView>
   );
 }
 
