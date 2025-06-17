7a3437cf061379d6ade780ab423c605ad60bf386
ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
2025-06-17 21:33:20 +0900

    변경된 위젯 디자인 적용

--- ios/MeditationBlossomWidget/MeditationBlossomWidget.swift
+++ ios/MeditationBlossomWidget/MeditationBlossomWidget.swift
@@ -49,22 +49,32 @@ struct MeditationBlossomWidgetEntryView : View {
   
   var body: some View {
     switch family {
-    case .systemSmall:
-      ZStack {
-        VStack{
-          Text("묵상")
-            .font(.system(size:20, weight:.bold))
-            .foregroundColor(.black)
-            .frame(width:320/3, height:30/3, alignment:.leading);
-          ZStack {
-            Image("background_364_382")
-              .resizable()
-            Text("또 비유로 말씀하시되 천국은 마치 여자가 가루 서 말 속에 갖다 넣어 전부 부풀게 한 누룩과 같으니라")
-              .font(.system(size:22, weight: .bold))
-              .foregroundColor(.black)
-              .frame(width:250/3, height:180/3)
-          }
+    case .systemLarge:
+      ZStack(alignment: .topLeading) {
+        Image("background_364_382")
+          .resizable()
+          .frame(width:364, height:382)
+        
+        VStack (alignment: .leading) {
+          Text("4.그리스도를 본받아")
+            .font(.system(size:20, weight: .bold))
+            .padding(.top)
+            .padding(.leading)
+          
+          Spacer().frame(height: 20)
+          
+          Text("11 그가 어떤 사람은 사도로, 어떤 사람은 선지자로, 어떤 사람은 복음 전하는 자로, 어떤 사람은 목사와 교사로 삼으셨으니")
+            .font(.system(size:18, weight: .bold))
+            .padding(.leading)
+            .padding(.trailing)
+          
+          Spacer().frame(height: 20)
+          
+          Text("마태복음 13:33")
+            .font(.system(size:16))
+            .padding(.leading)
         }
+        .padding(EdgeInsets(top: 30, leading: 30, bottom: 30, trailing: 30))
       }
     case .systemMedium:
       ZStack {
@@ -90,7 +100,7 @@ struct MeditationBlossomWidgetEntryView : View {
   }
 }
 
-#Preview(as: .systemMedium) {
+#Preview(as: .systemLarge) {
     MeditationBlossomWidget()
 } timeline: {
     SimpleEntry(date: .now)
