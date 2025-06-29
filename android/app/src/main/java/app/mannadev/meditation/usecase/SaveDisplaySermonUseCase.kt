62173807b145a7fad85d7232b84e113e3e6eb689
minchul <minchulkim.kr@gmail.com>
2025-06-29 09:07:36 +0900

    feature: prefs에 최신 말씀만 저장.

--- android/app/src/main/java/app/mannadev/meditation/usecase/SaveDisplaySermonUseCase.kt
+++ android/app/src/main/java/app/mannadev/meditation/usecase/SaveDisplaySermonUseCase.kt
@@ -3,12 +3,27 @@ package app.mannadev.meditation.usecase
 import app.mannadev.meditation.data.SermonDataSource
 import app.mannadev.meditation.di.PrefsDataSource
 import app.mannadev.meditation.dto.SermonDto
+import java.time.LocalDate
 import javax.inject.Inject
 
 class SaveDisplaySermonUseCase @Inject constructor(
     @PrefsDataSource private val prefsDataSource: SermonDataSource
 ) {
+
+    /**
+     * Saves the sermon to preferences if it is not older than the currently saved sermon.
+     */
     suspend operator fun invoke(sermon: SermonDto) {
-        prefsDataSource.saveDisplaySermon(sermon)
+        val oldSermon = prefsDataSource.getDisplaySermon()
+        if (oldSermon == null) {
+            prefsDataSource.saveDisplaySermon(sermon)
+            return
+        }
+
+        val oldDate = LocalDate.parse(oldSermon.date)
+        val newDate = LocalDate.parse(sermon.date)
+        if (!newDate.isBefore(oldDate)) {
+            prefsDataSource.saveDisplaySermon(sermon)
+        }
     }
 }
