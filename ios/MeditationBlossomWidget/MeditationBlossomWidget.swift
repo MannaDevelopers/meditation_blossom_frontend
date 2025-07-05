d90bebf1be49fdc3c79d984e078b2b43967f5b44
noni1016 <51592258+noni1016@users.noreply.github.com>
2025-07-05 14:27:35 +0900

    [SCRUM-20] React Native 에서 저장한 말씀 데이터 가져오기 (#32)
    * 위젯 재생성
    
    * ios 위젯 배경 적용
    
    * 위젯에 텍스트 추가
    
    * Podfile.lock update
    
    * 변경된 위젯 디자인 적용
    
    * AppGroup 추가
    
    * 설교를 앱 본체에서 가져오기
    
    * [ios]medium 위젯 앱 본체에서 가져온 말씀을 전시하도록 수정
    
    * [IOS] WidgetModuleUpdate를 통한 말씀 업데이트
    
    * [IOS] 중간크기 위젯 본문 세미볼드로 수정
    
    ---------
    
    Co-authored-by: ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
    Co-authored-by: SeminJang <pillar3500@gmail.com>

--- ios/MeditationBlossomWidget/MeditationBlossomWidget.swift
+++ ios/MeditationBlossomWidget/MeditationBlossomWidget.swift
@@ -7,40 +7,68 @@
 
 import WidgetKit
 import SwiftUI
+import Foundation
+import RegexBuilder
 
 struct SimpleEntry: TimelineEntry {
   let date: Date;
+  let title: String;
+  let quote: String;
+  let verse: String;
 }
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+  func placeholder(in context: Context) -> SimpleEntry {
+    SimpleEntry(date: Date(), title: " ", quote: "등록된 설교가 없습니다", verse: " ")
+  }
+  
+  func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    let entry = createSermonEntry()
+    completion(entry)
+  }
+  
+  func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
+    let entry = createSermonEntry()
+    let nextUpdate = Calendar.current.date(byAdding: .hour, value: 1, to: Date())!
+    let timeline = Timeline(entries: [entry], policy: .after(nextUpdate))
+    completion(timeline)
+  }
+  
+  private func createSermonEntry() -> SimpleEntry {
+    let sharedDefaults = UserDefaults(suiteName: "group.com.Blossom.MeditationBlossom")
+    
+    if let sermon = sharedDefaults?.getObjectFromString(forKey: "displaySermon", castTo: Sermon.self) {
+      var verse: String?
+      var quote: String?
+      
+      let verseRegex = /([가-힣]+\s?\d+:\d+)/
+      let quoteRegex = /\r\n\s*(.*)/
+      
+      if let match = sermon.content.firstMatch(of: verseRegex) {
+          verse = String(match.output.1)
+      }
+      if let match = sermon.content.firstMatch(of: quoteRegex) {
+          quote = String(match.output.1)
+      }
+      print(quote, verse)
+      return SimpleEntry(date: Date(), title: sermon.title, quote: quote ?? "설교 본문을 가져오는 중 문제가 발생했습니다", verse: verse ?? " ")
+    } else {
+      return SimpleEntry(date: Date(), title: " ", quote: "등록된 설교가 없습니다", verse: " ")
     }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        completion(SimpleEntry(date: Date()))
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
-        let currentDate = Date()
-        let nextUpdate = Calendar.current.date(byAdding: .minute, value: 15, to: currentDate)!
-        let entry = SimpleEntry(date: currentDate)
-        let timeline = Timeline(entries: [entry], policy: .after(nextUpdate))
-        completion(timeline)
-    }
+  }
 }
 
 
 struct MeditationBlossomWidget: Widget {
-    let kind: String = "MeditationBlossomWidget"
-
+  let kind: String = "MeditationBlossomWidget"
+  
   var body: some WidgetConfiguration {
-          StaticConfiguration(kind: "MyWidget", provider: Provider()) { entry in
-            MeditationBlossomWidgetEntryView(entry: entry)
-              .containerBackground(.fill.tertiary, for: .widget)
-          }
-        .supportedFamilies([.systemSmall, .systemMedium])
+    StaticConfiguration(kind: "MyWidget", provider: Provider()) { entry in
+      MeditationBlossomWidgetEntryView(entry: entry)
+        .containerBackground(.fill.tertiary, for: .widget)
     }
+    .supportedFamilies([.systemMedium, .systemLarge])
+  }
 }
 
 struct MeditationBlossomWidgetEntryView : View {
@@ -49,22 +77,35 @@ struct MeditationBlossomWidgetEntryView : View {
   
   var body: some View {
     switch family {
-    case .systemSmall:
-      ZStack {
-        VStack{
-          Text("묵상")
-            .font(.system(size:20, weight:.bold))
+    case .systemLarge:
+      ZStack(alignment: .topLeading) {
+        Image("background_364_382")
+          .resizable()
+          .frame(width:364, height:382)
+        
+        VStack (alignment: .leading) {
+          Text(entry.title)
+            .font(.system(size:20, weight: .bold))
             .foregroundColor(.black)
-            .frame(width:320/3, height:30/3, alignment:.leading);
-          ZStack {
-            Image("background_364_382")
-              .resizable()
-            Text("또 비유로 말씀하시되 천국은 마치 여자가 가루 서 말 속에 갖다 넣어 전부 부풀게 한 누룩과 같으니라")
-              .font(.system(size:22, weight: .bold))
-              .foregroundColor(.black)
-              .frame(width:250/3, height:180/3)
-          }
+            .padding(.top)
+            .padding(.leading)
+          
+          Spacer().frame(height: 20)
+          
+          Text(entry.quote)
+            .font(.system(size:18, weight: .semibold))
+            .foregroundColor(.black)
+            .padding(.leading)
+            .padding(.trailing)
+          
+          Spacer().frame(height: 20)
+          
+          Text(entry.verse)
+            .font(.system(size:16))
+            .foregroundColor(.black)
+            .padding(.leading)
         }
+        .padding(EdgeInsets(top: 30, leading: 30, bottom: 30, trailing: 30))
       }
     case .systemMedium:
       ZStack {
@@ -72,12 +113,12 @@ struct MeditationBlossomWidgetEntryView : View {
           .resizable()
           .frame(width:364, height:170);
         VStack{
-          Text("또 비유로 말씀하시되 천국은 마치 여자가 가루 서 말 속에 갖다 넣어 전부 부풀게 한 누룩과 같으니라")
-            .font(.system(size:22, weight: .bold))
+          Text(entry.quote)
+            .font(.system(size:22, weight: .semibold))
             .foregroundColor(.black)
             .frame(width:300, height:100)
             .offset(y:3)
-          Text("마태복음 13:33")
+          Text(entry.verse)
             .font(.system(size:15))
             .foregroundColor(.black)
             .frame(width:180, height:20, alignment:.trailing)
@@ -91,8 +132,7 @@ struct MeditationBlossomWidgetEntryView : View {
 }
 
 #Preview(as: .systemMedium) {
-    MeditationBlossomWidget()
+  MeditationBlossomWidget()
 } timeline: {
-    SimpleEntry(date: .now)
-    SimpleEntry(date: .now)
+  SimpleEntry(date: Date(), title: " ", quote: "등록된 설교가 없습니다", verse: " ")
 }
