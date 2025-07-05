76bbd41e7cc61515d26043b0280e8184c6d3eeb5
noni1016 <51592258+noni1016@users.noreply.github.com>
2025-07-05 14:27:35 +0900

    [SCRUM-20] React Native 에서 저장한 말씀 데이터 가져오기 (#32)
    * 위젯 재생성
    
    * ios 위젯 배경 적용
    
    * 위젯에 텍스트 추가
    
    * Podfile.lock update
    
    * 변경된 위젯 디자인 적용
    
    * AppGroup 추가
    
    * 설교를 앱 본체에서 가져오기
    
    * [ios]medium 위젯 앱 본체에서 가져온 말씀을 전시하도록 수정
    
    * [IOS] WidgetModuleUpdate를 통한 말씀 업데이트
    
    * [IOS] 중간크기 위젯 본문 세미볼드로 수정
    
    ---------
    
    Co-authored-by: ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
    Co-authored-by: SeminJang <pillar3500@gmail.com>

---
+++ ios/MeditationBlossomWidget/Sermon.swift
@@ -0,0 +1,57 @@
+//
+//  Sermon.swift
+//  meditation_blossom
+//
+//  Created by 최상준 on 6/17/25.
+//
+import Foundation
+
+struct FirestoreTimeStamp: Codable {
+  let seconds: Int64
+  let nanoseconds: Int32
+}
+
+struct Sermon: Codable {
+    let id: String
+    let title: String
+    let content: String
+    let date: String
+    let category: String? // Optional
+    let dayOfWeek: String? // Optional + 이름 변경
+    let createdAt: FirestoreTimeStamp // 이름 변경
+    let updatedAt: FirestoreTimeStamp // 이름 변경
+
+    // JSON의 키 이름과 Swift 구조체의 프로퍼티 이름을 매핑합니다.
+    // 이름이 동일한 경우는 생략해도 되지만, 명시적으로 모두 작성하는 것이 좋습니다.
+    enum CodingKeys: String, CodingKey {
+        case id, title, content, date, category
+        case dayOfWeek = "day_of_week"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
+
+extension UserDefaults {
+    // JSON String을 Codable 객체로 변환하여 불러오는 함수
+    func getObjectFromString<T: Codable>(forKey key: String, castTo type: T.Type) -> T? {
+        // UserDefaults에서 저장된 JSON 문자열을 가져옴
+        guard let jsonString = self.string(forKey: key) else {
+            print("'\(key)'에 해당하는 문자열을 찾을 수 없습니다.")
+            return nil
+        }
+        guard let data = jsonString.data(using: .utf8) else {
+              print("문자열을 Data로 변환하는 데 실패했습니다.")
+              return nil
+        }
+        print(jsonString)
+        // Swift 객체로 디코딩
+        do {
+          let decoder = JSONDecoder()
+          let object = try decoder.decode(type, from: data)
+            return object
+        } catch {
+            print("JSON 디코딩 실패: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
