b45f1d89301826e8b5274117efc94e3d0c20a697
minchul <minchulkim.kr@gmail.com>
2025-08-10 16:53:09 +0900

    fix: 위젯 설명 수정.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "app.mannadev.meditation"
         minSdk = rootProject.extra["minSdkVersion"].toString().toInt()
         targetSdk = rootProject.extra["targetSdkVersion"].toString().toInt()
-        versionCode = 2
-        versionName = "0.4.3"  // 버전 업데이트
+        versionCode = 4
+        versionName = "0.4.5"  // 버전 업데이트
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
