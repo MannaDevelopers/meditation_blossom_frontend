8b79e79ed4774d511b2b4a7f1353859196f27fcf
KyeongeunKim <play.noni@gmail.com>
2026-08-10 11:25:00 +0900

    [ISSUE-214] feat: 배경 갤러리 이미지 피커 + 크롭/포지션 조정 화면

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,6 +1,14 @@
 import { Sermon } from "./Sermon";
+import { WidgetImageTransform } from "./WidgetDesign";
+
+export type ImageCropResult = {
+  uri: string;
+  transform: WidgetImageTransform;
+};
+
 export type RootStackParamList = {
   MainTabs: undefined;
-  EditScreen: { sermon?: Sermon };
+  EditScreen: { sermon?: Sermon; cropResult?: ImageCropResult };
   SettingsScreen: undefined;
+  ImageCropScreen: { imageUri: string; initialTransform?: WidgetImageTransform };
 };
